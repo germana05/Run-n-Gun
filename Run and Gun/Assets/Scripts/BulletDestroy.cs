7c56b5ed114316610dcfc3e7033c38dc33055ead
marshall verboekend <marshalllyron@gmail.com>
2024-05-29 09:07:51 +0200

    nothing

--- Run and Gun/Assets/Scripts/BulletDestroy.cs
+++ Run and Gun/Assets/Scripts/BulletDestroy.cs
@@ -20,4 +20,9 @@ public class BulletDestroy : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
